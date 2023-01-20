2c8c89f4485b3493994b32b70b68c6a274cc4160
Souradip pal <105942415+SparklinStar@users.noreply.github.com>
2023-01-20 22:18:48 +0530

    2 cp problem done

---
+++ Domino_piling.cpp
@@ -0,0 +1,10 @@
+// http://codeforces.com/problemset/problem/50/A
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a * b / 2 << endl;
+}
